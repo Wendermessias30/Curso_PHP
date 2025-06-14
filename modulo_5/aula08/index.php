760c5e12b3a48bbf2135322c5226f2809d450ac0
Wender Messias <132120227+Wendermessias30@users.noreply.github.com>
2025-06-14 17:12:58 -0300

    atualição

---
+++ modulo_5/aula08/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  $numero1 = 5+5;
+  $numero2 = 10-4;
+
+  $resultado = $numero1 + $numero2;
+  $resultado2 =  10/5;
+  // $numero -= 5;
+  // $numero *= 2;
+  // $numero /= 2;
+  echo $resultado2;
+
+?> 
